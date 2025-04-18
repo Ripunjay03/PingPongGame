0d82f8a23798ca108d6d962857d0e80f49956606
Ripunjay <ripunjay.2003@gmail.com>
2025-04-18 17:56:46 +0530

    Initial commit

---
+++ android/app/src/main/java/pong/dileep/com/MainActivity.java
@@ -0,0 +1,5 @@
+package pong.dileep.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/index.js
@@ -0,0 +1,1 @@
+// This file is intentionally left blank to avoid loading backend code in the frontend on Android.
